845ebf0d44364755528a74079f4f36067faccf9a
Luis De Gracia <ldegracia@rootstack.com>
2022-05-08 15:01:30 -0500

    Setup path alias also added files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
-import '../styles/index.css'
+import '../../public/styles/index.css'
 
 function JobListingApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
